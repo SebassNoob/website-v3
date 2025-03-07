a020786d7cba025435a3f4893d1cdc35e08c275f
SebassNoob <sebastian.ong@hotmail.com>
2025-03-08 03:25:33 +0800

    fix: error handling for no commit sha

--- src/scripts/generateBuildInfo.ts
+++ src/scripts/generateBuildInfo.ts
@@ -3,7 +3,18 @@ import { writeFileSync } from "node:fs";
 import { execSync } from "node:child_process";
 import type { BuildInfo } from "@lib/actions";
 
-const commitSHA = execSync("git rev-parse --short HEAD").toString().trim();
+
+let commitSHA: string;
+try {
+  commitSHA = execSync("git rev-parse --short HEAD").toString().trim();
+} catch (error) {
+  console.warn("⚠️ Failed to retrieve commit SHA, falling back to 'Unknown commit SHA'");
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+  commitSHA = "Unknown commit SHA";
+}
+
 
 const buildInfo = {
 	lastUpdated: new Date().toISOString(),
