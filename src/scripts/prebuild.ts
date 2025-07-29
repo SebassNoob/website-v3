bb9e184b22c6cd8cbbf3f642a1dfcb1994b87486
SebassNoob <78428559+SebassNoob@users.noreply.github.com>
2025-07-29 06:17:05 +0000

    fix: generate metadata on dev server start

--- src/scripts/prebuild.ts
+++ src/scripts/prebuild.ts
@@ -3,8 +3,16 @@ import { GENERATED_DIR } from "@lib/constants";
 import { generateBuildInfo } from "./generateBuildInfo";
 import { generateJsonLd } from "./generateJsonLd";
 
+// process flags
+const NO_CACHE = process.argv.includes("--no-cache");
+
+if (!NO_CACHE && existsSync(GENERATED_DIR)) {
+	console.info(`ℹ️ ${GENERATED_DIR} exists, skipping...`);
+	process.exit(0);
+}
+
 if (!existsSync(GENERATED_DIR)) {
-	console.warn("⚠️ '/public/generated' directory does not exist, creating it...");
+	console.warn(`⚠️ ${GENERATED_DIR} directory does not exist, creating it...`);
 	mkdirSync(GENERATED_DIR);
 }
 
