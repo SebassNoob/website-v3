475f887b7b2cc82aeb528df91dac8662faf0b87f
SebassNoob <sebastian.ong@hotmail.com>
2025-10-11 11:58:39 +0800

    fix: update prebuild to force recreation of generated dir

--- src/scripts/prebuild.ts
+++ src/scripts/prebuild.ts
@@ -1,4 +1,4 @@
-import { mkdirSync, existsSync } from "node:fs";
+import { mkdirSync, existsSync, rmSync } from "node:fs";
 import { GENERATED_DIR } from "@lib/constants";
 import { generateBuildInfo } from "./generateBuildInfo";
 import { generateJsonLd } from "./generateJsonLd";
@@ -10,7 +10,8 @@ if (existsSync(GENERATED_DIR) && !FORCE) {
 	console.info(`ℹ️ ${GENERATED_DIR} exists, skipping...`);
 	process.exit(0);
 } else {
-	console.warn(`⚠️ Creating ${GENERATED_DIR}...`);
+	console.warn(`⚠️ Recreating ${GENERATED_DIR}...`);
+  rmSync(GENERATED_DIR, { recursive: true, force: true });
 	mkdirSync(GENERATED_DIR);
 }
 
